cefe3bbd1a6bb28b0de7ce6e9c20ac31954a27b3
misael-gdev <mslsanchez@hotmail.com>
2026-01-24 18:14:12 +0100

    Set default values for transitions in State Machines

--- Runtime/ai/behaviour/statemachines/IStateMachine.cs
+++ Runtime/ai/behaviour/statemachines/IStateMachine.cs
@@ -4,9 +4,9 @@ namespace TSLib.AI.Behaviour.StateMachines
     {
         public void Execute(float deltaTime);
 
-        public void TransitionTo(State newState, bool doEnter, bool doExit, bool allowSameState);
+        public void TransitionTo(State newState, bool doEnter = true, bool doExit = true, bool allowSameState = false);
 
-        public void RevertToPrevious(bool doEnter, bool doExit, bool allowSameState);
+        public void RevertToPrevious(bool doEnter = false, bool doExit = true, bool allowSameState = false);
 
         public bool IsSameState(State s1, State s2);
     }

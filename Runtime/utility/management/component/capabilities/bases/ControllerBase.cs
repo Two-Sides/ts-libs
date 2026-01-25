4e29e926cbdbe9acde385c9835cc77f4b4137abb
misael-gdev <mslsanchez@hotmail.com>
2026-01-25 17:39:01 +0100

    Auto init, configure and deconfigure for controllers

--- Runtime/utility/management/component/capabilities/bases/ControllerBase.cs
+++ Runtime/utility/management/component/capabilities/bases/ControllerBase.cs
@@ -1,8 +1,54 @@
+using System;
+using UnityEngine;
+
 namespace TSLib.Utility.Management.Component.Capabilities
 {
     public abstract class ControllerBase : ComponentBase
     {
+        [SerializeField] protected ComponentBase[] Components;
+
         public virtual void Register() { }
         public virtual void Unregister() { }
+
+        public override void Initialize()
+        {
+            if (Components == null)
+                throw new ArgumentNullException(nameof(Components));
+
+            for (int i = 0; i < Components.Length; i++)
+            {
+                var component = Components[i];
+                if (component == null) continue;
+                component.Initialize();
+            }
+        }
+
+        public override void Configure()
+        {
+            if (Components == null)
+                throw new ArgumentNullException(nameof(Components));
+
+            for (int i = 0; i < Components.Length; i++)
+            {
+                var component = Components[i];
+                if (component == null) continue;
+                component.Configure();
+            }
+        }
+
+        public override void Deconfigure()
+        {
+            for (int i = 0; i < Components.Length; i++)
+            {
+                var component = Components[i];
+                if (component == null) continue;
+
+                component.Deconfigure();
+                Components[i] = null;
+            }
+
+            Components = null;
+        }
+
     }
 }

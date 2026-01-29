8be29539587de31d72fbad9ea77d2050d4035393
misael-gdev <mslsanchez@hotmail.com>
2026-01-29 18:16:10 +0100

    Set some namespaces

--- Runtime/utility/patterns/singleton/Singleton.cs
+++ Runtime/utility/patterns/singleton/Singleton.cs
@@ -1,50 +1,54 @@
 using TSLib.Utility.Management.Component.Capabilities;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace TSLib.Utility.Patterns.Singleton
 {
-    protected virtual bool Persistent => true;
-    protected static T instance;
-    private static bool _isQuitting;
-
-    public static T Instance
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        protected virtual bool Persistent => true;
+        protected static T instance;
+        private static bool _isQuitting;
+
+        public static T Instance
         {
-            if (_isQuitting) return null;
+            get
+            {
+                if (_isQuitting) return null;
 
-            if (instance != null) return instance;
+                if (instance != null) return instance;
 
-            instance = FindFirstObjectByType<T>(FindObjectsInactive.Include);
+                instance = FindFirstObjectByType<T>(FindObjectsInactive.Include);
 
-            return instance;
-        }
-    }
-
-    protected virtual void Awake()
-    {
-        if (!Application.isPlaying) return;
-
-        if (instance == null)
-        {
-            instance = this as T;
-
-            if (Persistent) DontDestroyOnLoad(gameObject);
-
-            OnSingletonAwake();
-            return;
+                return instance;
+            }
         }
 
-        if (instance != this) Destroy(gameObject);
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying) return;
+
+            if (instance == null)
+            {
+                instance = this as T;
+
+                if (Persistent) DontDestroyOnLoad(gameObject);
+
+                OnSingletonAwake();
+                return;
+            }
+
+            if (instance != this) Destroy(gameObject);
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (instance != this) return;
+            instance = null;
+        }
+
+        protected virtual void OnApplicationQuit() => _isQuitting = true;
+
+        protected virtual void OnSingletonAwake() { }
     }
-
-    protected virtual void OnDestroy()
-    {
-        if (instance != this) return;
-        instance = null;
-    }
-
-    protected virtual void OnApplicationQuit() => _isQuitting = true;
-
-    protected virtual void OnSingletonAwake() { }
 }
+

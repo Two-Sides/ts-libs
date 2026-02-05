00c730ddbab77151e8e060b033d121ef8016e145
misael-gdev <mslsanchez@hotmail.com>
2026-02-05 13:54:58 +0100

    Optional constructor and validations for Transitions

--- Runtime/ai/behaviour/statemachines/PHFSM/Transition.cs
+++ Runtime/ai/behaviour/statemachines/PHFSM/Transition.cs
@@ -5,9 +5,17 @@ namespace TSLib.AI.Behaviour.StateMachines.PHFSM
     public class Transition
     {
         public PHS NextState { get; private set; }
+
+        public Transition() { }
+        public Transition(PHS nextState) => SetNextState(nextState);
+
         public void SetNextState(PHS nextState)
         {
             if (nextState == null) throw new ArgumentNullException(nameof(nextState));
+
+            if (NextState != null) throw new InvalidOperationException(
+                "(invalid) NextState is already assinged.");
+
             NextState = nextState;
         }
 
